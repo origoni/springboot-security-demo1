25e697cfb87acfa8709327c0a69f31ac050b0f74
DK Lee <origoni@live.com>
2018-06-12 21:47:42 +0900

    Update SecurityConfig.java

--- src/main/java/com/millky/demo/security/one/SecurityConfig.java
+++ src/main/java/com/millky/demo/security/one/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.millky.demo.security.one;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,6 +8,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    // It is almost the same as the default.
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // @formatter:off
@@ -20,12 +20,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         // @formatter:on
     }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
         // @formatter:off
         auth
             .inMemoryAuthentication()
                 .withUser("user").password("{noop}password").roles("USER");
         // @formatter:on
     }
+
+    // If you do not extend WebSecurityConfigurerAdapter ...
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//        // @formatter:off
+//        auth
+//            .inMemoryAuthentication()
+//                .withUser("user").password("{noop}password").roles("USER");
+//        // @formatter:on
+//    }
 }
